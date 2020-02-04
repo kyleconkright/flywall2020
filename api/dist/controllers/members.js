3d3e39ae4dd97f2790761118be0daaf7be3bc5d6
Kyle Conkright <kyleconkright@gmail.com>
2020-02-03 21:29:02 -0800

    ignore dist

--- api/dist/controllers/members.js
+++ api/dist/controllers/members.js
@@ -57,9 +57,7 @@ function getCompareMembers(request, response) {
             if (!congressNumber || !chamber) {
                 throw new Error("Missing a chamber or congress number");
             }
-            console.log(member1, member2, congressNumber, chamber);
             const res = yield axios_1.default.get(propublica.compareMembers(member1, member2, congressNumber, chamber), propublica.headers);
-            console.log("compare >> ", res.data.results);
             response.json({ data: res.data.results });
         }
         catch (err) {
