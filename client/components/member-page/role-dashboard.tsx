57225bee5d0c6f85d1d25e936f042015393f203f
Kyle Hurst <kylehurst42@gmail.com>
2020-02-03 18:37:55 -0700

    change where charts are

--- client/components/member-page/role-dashboard.tsx
+++ client/components/member-page/role-dashboard.tsx
@@ -132,6 +132,53 @@ export function RoleDashboard(props: Props) {
           />
         </div>
         <div style={chartSizes}>
+          <ResponsivePie
+            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+            innerRadius={0.5}
+            padAngle={3}
+            cornerRadius={5}
+            borderWidth={1}
+            borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
+            slicesLabelsSkipAngle={30}
+            slicesLabelsTextColor="#fff"
+            animate
+            colors={pieColors}
+            data={[
+              {
+                id: "votes_for",
+                label: "Votes For",
+                value: role.votes_with_party_pct
+              },
+              {
+                id: "votes_against",
+                label: "Votes Against",
+                value: role.votes_against_party_pct
+              }
+            ]}
+            legends={[
+              {
+                anchor: "bottom",
+                direction: "row",
+                translateY: 56,
+                itemWidth: 100,
+                itemHeight: 18,
+                itemTextColor: "#999",
+                symbolSize: 18,
+                symbolShape: "circle",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemTextColor: "#000"
+                    }
+                  }
+                ]
+              }
+            ]}
+          />
+        </div>
+
+        <div style={chartSizes}>
           <ResponsiveBar
             colors={{ scheme: "purple_blue" }}
             data={[
@@ -223,53 +270,6 @@ export function RoleDashboard(props: Props) {
             ]}
           />
         </div>
-        <div style={chartSizes}>
-          <ResponsivePie
-            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-            innerRadius={0.5}
-            padAngle={3}
-            cornerRadius={5}
-            borderWidth={1}
-            borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
-            slicesLabelsSkipAngle={30}
-            slicesLabelsTextColor="#fff"
-            animate
-            colors={pieColors}
-            data={[
-              {
-                id: "votes_for",
-                label: "Votes For",
-                value: role.votes_with_party_pct
-              },
-              {
-                id: "votes_against",
-                label: "Votes Against",
-                value: role.votes_against_party_pct
-              }
-            ]}
-            legends={[
-              {
-                anchor: "bottom",
-                direction: "row",
-                translateY: 56,
-                itemWidth: 100,
-                itemHeight: 18,
-                itemTextColor: "#999",
-                symbolSize: 18,
-                symbolShape: "circle",
-                effects: [
-                  {
-                    on: "hover",
-                    style: {
-                      itemTextColor: "#000"
-                    }
-                  }
-                ]
-              }
-            ]}
-          />
-        </div>
-        <hr></hr>
       </div>
     </div>
   );
