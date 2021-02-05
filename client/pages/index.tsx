c1f948fa53779c128769ee080c3bfca2125354e3
Kyle Hurst <kylehurst42@gmail.com>
2021-02-04 19:58:32 -0700

    add title to home page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import Head from "next/head";
+import { TabTitle } from "../components/head/head";
 interface Props {}
 
 const Home: React.FC<Props> = () => {
-  return <div>Welcome to Flywall</div>;
+  return (
+    <div>
+      <TabTitle title="FlyWall - Home" />
+      Welcome to Flywall
+    </div>
+  );
 };
 
 export default Home;
